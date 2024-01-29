18577de69fec4106d6e9d5ea1a3db124883f479d
Danilo Souza <danilobrayann@gmail.com>
2024-01-29 12:28:55 -0300

    feat: implementando formulario

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { BntPrimaryComponent } from '../bnt-primary/bnt-primary.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
   standalone: true,
-  imports: [BntPrimaryComponent],
+  imports: [
+    BntPrimaryComponent,
+  ReactiveFormsModule
+  ],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
