17846c59a0e6ed22222776ea774586739d58b9fa
Danilo Souza <danilobrayann@gmail.com>
2024-01-30 12:11:52 -0300

    feat: atulizações de novos cards

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import { BntPrimaryComponent } from '../bnt-primary/bnt-primary.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
   standalone: true,
   imports: [
     BntPrimaryComponent,
-  ReactiveFormsModule
+    ReactiveFormsModule
   ],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
+
+  newsletterForm!: FormGroup;
+
+  constructor() {
+    this.newsletterForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+
+  Onsubmitt(){
+    console.log(this.newsletterForm.value)
+}
 
 }
